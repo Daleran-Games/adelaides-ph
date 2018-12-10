67f2feec3cc3c4418b931b2c8b84d2de6bb4858b
daleran <daleran@gmail.com>
2018-12-10 10:10:30 +0900

    Imported project

---
+++ Assets/DalLib/PixelArt/Scripts/PixelPerfect/PixelPerfectCamera.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace DaleranGames.PixelArt
+{
+    [AddComponentMenu("Rendering/Pixel Perfect Camera")]
+    [RequireComponent(typeof(Camera))]
+    [ExecuteInEditMode]
+    public class PixelPerfectCamera : MonoBehaviour
+    {
+        [SerializeField]
+        int pixelsPerUnit = 1;
+        public float PixelsPerUnit { get { return pixelsPerUnit; } }
+
+        float unitsInPixels;
+        public float UnitsInPixel { get { return unitsInPixels; } }
+
+        [SerializeField]
+        bool scaleOnStart = true;
+
+        [SerializeField]
+        [Range(1,20)]
+        int scale = 1;
+        public int Scale
+        {
+            get { return scale; }
+            set
+            {
+                if (value > 0)
+                {
+                    scale = value;
+                    ScaleCamera();
+                    ScaleChanged?.Invoke(scale); 
+                }
+            }
+        }
+        public event Action<int> ScaleChanged;
+
+        protected Camera cam;
+        // Use this for initialization
+        private void Awake()
+        {
+            cam = gameObject.GetRequiredComponent<Camera>();
+            unitsInPixels = 1 / pixelsPerUnit;
+        }
+
+        void Start()
+        {
+            if (scaleOnStart)
+                ScaleCamera();
+        }
+
+        private void OnValidate()
+        {
+            unitsInPixels = 1 / pixelsPerUnit;
+            ScaleCamera();
+        }
+
+        protected virtual float CalculateOrthographicSize(float scale)
+        {
+            return (Screen.height / (scale * PixelsPerUnit)) * 0.5f;
+        }
+
+        [ContextMenu("Scale Camera")]
+        public void ScaleCamera()
+        {
+            if (cam == null)
+                cam = gameObject.GetRequiredComponent<Camera>();
+
+            ScaleCamera(Scale);
+        }
+
+        public void ScaleCamera(int scale)
+        {
+            cam.orthographicSize = CalculateOrthographicSize(scale);
+        }
+
+    }
+}
+
---
+++ Assets/DalLib/PixelArt/Scripts/PixelPerfect/SnapToPixels.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DaleranGames.PixelArt
+{
+    [AddComponentMenu("Rendering/Pixel Snap")]
+    public class SnapToPixels : MonoBehaviour
+    {
+
+        public float PixelsPerUnit = 16; // pixels per unit (your tile size)
+
+        private void LateUpdate()
+        {
+            Vector3 position = transform.localPosition;
+            position.x = (Mathf.Round(transform.parent.position.x * PixelsPerUnit) / PixelsPerUnit) - transform.parent.position.x;
+            position.y = (Mathf.Round(transform.parent.position.y * PixelsPerUnit) / PixelsPerUnit) - transform.parent.position.y;
+            transform.localPosition = position;
+        }
+
+    }
+}
